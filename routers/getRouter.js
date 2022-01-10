520a526fda3fb9d211a89680208c72a2e3357611
EhoodGoren <ehoodgoren@gmail.com>
2022-01-10 21:40:07 +0200

    Added get endpoints for all object types

--- routers/getRouter.js
+++ routers/getRouter.js
@@ -14,6 +14,45 @@ router.get('/student/:id', (req, res) => {
         if(err) throw err;
         res.send(result);
     })
-})
+});
+
+router.get('/class/:id', (req, res) => {
+    const classId = req.params.id;
+    const sql = `
+    select *
+     from Classes
+     where classId = ${classId}
+    `;
+    con.query(sql, (err, result) => {
+        if(err) throw err;
+        res.send(result);
+    })
+});
+
+router.get('/teacher/:id', (req, res) => {
+    const teacherId = req.params.id;
+    const sql = `
+    select *
+     from Teachers
+     where teacherId = ${teacherId}
+    `;
+    con.query(sql, (err, result) => {
+        if(err) throw err;
+        res.send(result);
+    })
+});
+
+router.get('/subject/:id', (req, res) => {
+    const subjectId = req.params.id;
+    const sql = `
+    select *
+     from Subjects
+     where subjectId = ${subjectId}
+    `;
+    con.query(sql, (err, result) => {
+        if(err) throw err;
+        res.send(result);
+    })
+});
 
 module.exports = router;
