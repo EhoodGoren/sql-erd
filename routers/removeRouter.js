7d58f88d8fb3984563f6b7b15e338c406e49aff0
EhoodGoren <ehoodgoren@gmail.com>
2022-01-10 22:31:46 +0200

    Added endpoints for deleting students and teachers

--- routers/removeRouter.js
+++ routers/removeRouter.js
@@ -1,5 +1,28 @@
 const express = require('express');
+const con = require('../sql-connection');
 
 const router = express.Router();
+
+router.delete('/student/:id', (req, res) => {
+    const studentId = req.params.id;
+    const sql = `
+    delete from Students where studentId = ${studentId}
+    `;
+    con.query(sql, (err, result) => {
+        if(err) throw err;
+        else res.send('Deleted successfully');
+    })
+});
+
+router.delete('/teacher/:id', (req, res) => {
+    const teacherId = req.params.id;
+    const sql = `
+    delete from Teachers where teacherId = ${teacherId}
+    `;
+    con.query(sql, (err, result) => {
+        if(err) throw err;
+        else res.send('Deleted successfully');
+    })
+});
 
 module.exports = router;
